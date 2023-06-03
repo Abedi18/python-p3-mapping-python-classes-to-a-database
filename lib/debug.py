66c311287c2db0a412c699813745acfa907a5976
Abednego <abedikroos15@gmail.com>
2023-06-03 15:45:01 +0300

    Mapped Python Classes to a Database

--- lib/debug.py
+++ lib/debug.py
@@ -3,6 +3,17 @@
 from config import CONN, CURSOR
 from song import Song
 
+def main():
+    Song.create_table()
+
+    blinding_lights = Song("Blinding Lights", "After Hours")
+    blinding_lights.save()
+
+    hello = Song("Hello", "25")
+    hello.save()
+
+    print("Songs saved successfully!")
 
 if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+    main()
+
